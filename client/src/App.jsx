6b89e5a09be90ceecdc23903fff9bae5332a769b
7chyzxkcrk-rgb <7chyzxkcrk@privaterelay.appleid.com>
2026-04-17 18:53:29 +0900

    최대 너비 제한 흰색 여백

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import Header from './components/Header'
-import HeroSection from './components/HeroSection'
 import MainCards from './components/MainCards'
 import QuickMenu from './components/QuickMenu'
 import IntroSection from './components/IntroSection'
@@ -9,15 +8,16 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <HeroSection />
-      <MainCards />
-      <QuickMenu />
-      <IntroSection />
-      <ContentSection />
-      <ServiceMap />
-      <Footer />
+    <div className="bg-white min-h-screen">
+      <div className="max-w-4xl mx-auto">
+        <Header />
+        <MainCards />
+        <QuickMenu />
+        <IntroSection />
+        <ContentSection />
+        <ServiceMap />
+        <Footer />
+      </div>
     </div>
   )
 }
