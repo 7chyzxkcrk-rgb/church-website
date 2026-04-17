e26bdc3a32b1b43e3e40c131db3bf831d26d6726
7chyzxkcrk-rgb <7chyzxkcrk@privaterelay.appleid.com>
2026-04-17 21:16:41 +0900

    최대 너비 1280px로 조정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="bg-white min-h-screen">
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-7xl mx-auto">
         <Header />
         <MainCards />
         <QuickMenu />
