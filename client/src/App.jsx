acb02b66afcf29c961c75a8b05cc92361979734a
7chyzxkcrk-rgb <7chyzxkcrk@privaterelay.appleid.com>
2026-04-17 21:27:23 +0900

    최대 너비 1400px로 조정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="bg-white min-h-screen">
-      <div className="max-w-7xl mx-auto">
+      <div style={{maxWidth: '1400px'}} className="mx-auto">
         <Header />
         <MainCards />
         <QuickMenu />
